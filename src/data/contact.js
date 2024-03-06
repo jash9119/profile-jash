9487e9bcba1633d78e0caaf390794f29370e4c8d
Jaya Shree Hada <jayashreehada@Jayas-MacBook-Pro.local>
2024-03-07 03:49:21 +0700

    contactdata

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,16 +1,16 @@
 export default [
     {
-        imgUrl: 'src/assets/Email.png',
+        imgUrl: '/assets/Email.png',
         
         link: "https://mail.google.com/mail/?view=cm&to=jayashhada@gmail.com",
     },
     {
-        imgUrl: 'src/assets/git.png',
+        imgUrl: '/assets/git.png',
         
         link: 'https://github.com/jash9119',
     },
     {
-        imgUrl: 'src/assets/linkedin.png',
+        imgUrl: '/assets/linkedin.png',
         link: 'https://www.linkedin.com/in/jaya-shree-hada-2a66b3194',
     },
 ]
