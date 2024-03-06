777362321dc615c160e3fe89b24ddf7a7d551483
Jaya Shree Hada <jayashreehada@Jayas-MacBook-Pro.local>
2024-03-07 03:58:32 +0700

    imgdata

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,16 +1,16 @@
 export default [
     {
-        imgUrl: '/assets/Email.png',
+        imgUrl: './assets/Email.png',
         
         link: "https://mail.google.com/mail/?view=cm&to=jayashhada@gmail.com",
     },
     {
-        imgUrl: '/assets/git.png',
+        imgUrl: './assets/git.png',
         
         link: 'https://github.com/jash9119',
     },
     {
-        imgUrl: '/assets/linkedin.png',
+        imgUrl: './assets/linkedin.png',
         link: 'https://www.linkedin.com/in/jaya-shree-hada-2a66b3194',
     },
 ]
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Badminton App Design',
-        imgUrl: 'src/assets/tag_in.png',
+        imgUrl: './assets/tag_in.png',
         stack: ['Figma', 'HCI', 'UI/UX'],
         link: 'https://www.figma.com/proto/mrY2Qo8PRHenQtT5H8sIa3/HCI-v1.0?type=design&node-id=21-687&t=XaHDJbN3dj12RiZI-1&scaling=scale-down&page-id=0%3A10&starting-point-node-id=0%3A11&mode=design',
     },
     {
         title: 'Amusigo',
-        imgUrl: 'src/assets/Amusigo.png',
+        imgUrl: './assets/Amusigo.png',
         stack: ['HTML/CSS', 'PHP', 'SQL'],
         link: 'https://github.com/jash9119/DTB_project.git',
     },
     {
         title: 'QPromo Logo Design',
-        imgUrl: 'src/assets/Qpromo.png',
+        imgUrl: './assets/Qpromo.png',
         stack: ['Figma', 'HCI', 'UI/UX'],
         link: 'src/components/error.html',
     },
